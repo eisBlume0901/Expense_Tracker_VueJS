2047f47b5e2390408b5baa383502c06c52841e93
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-01-14 18:46:44 +0800

    Added template content

---
+++ expense_tracker/src/components/Balance.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="balance">
+    <h2>Your Balance</h2>
+    <h3>${balance}</h3>
+  </div>
+</template>
---
+++ expense_tracker/src/components/IncomeExpenses.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="inc-exp-container">
+    <div>
+      <h4>Income</h4>
+      <p id="money-plus" class="money plus">+Php{{ income }}</p>
+    </div>
+    <div>
+      <h4>Expense</h4>
+      <p id="money-minus" class="money minus">-Php{{ expenses }}</p>
+    </div>
+  </div>
+</template>
